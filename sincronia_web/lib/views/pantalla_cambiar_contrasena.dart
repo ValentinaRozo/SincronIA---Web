f212478943856691a1aaedd8b7060995b8126651
Valentina Rozo <valerozobernal@gmail.com>
2024-09-22 21:42:43 -0500

    esqueleto cambiar contraseña

---
+++ sincronia_web/lib/views/pantalla_cambiar_contrasena.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PantallaCambiarContrasena extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Pantalla para cambiar contraseña',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
